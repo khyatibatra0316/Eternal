534196ecd6f136d6c59c8408392069a9df6cfb58
Khyati Batra <batrakhyati50@gmail.com>
2025-09-22 11:48:35 +0530

    second

--- Eternal Dhamma/src/App.jsx
+++ Eternal Dhamma/src/App.jsx
@@ -1,8 +1,9 @@
+import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="app-container">
+      <h1 className="app-title">Welcome to Eternal Dhamma!!</h1>
     </div>
   );
 }
